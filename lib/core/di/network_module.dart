93fb5a41d30ce581169382f7426694bef8c0b9dd
Hajir Mujib <mujibhajir@gmail.com>
2024-06-07 15:46:08 +0700

    - Connect super module && generator

--- lib/core/di/network_module.dart
+++ lib/core/di/network_module.dart
@@ -1,6 +1,5 @@
-import 'package:base_pkg/base_pkg.dart';
 import 'package:bloc_skeleton/core/data/remote/interceptors/auth_interceptor.dart';
-
+import 'package:injectable/injectable.dart';
 
 @module
 abstract class NetworkModule {
